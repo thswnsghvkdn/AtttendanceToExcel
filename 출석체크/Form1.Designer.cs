7921cbf40b9322b79ba7949e926b5034125f3476
thswnsghvkdn <60654232+thswnsghvkdn@users.noreply.github.com>
2020-08-14 10:38:43 +0900

    출석체크 프로토타입
    출석명단에 명단을 작성하면 해당 이름에 출석
    이름이 외자일경우 예외처리
    로딩 표시

---
+++ 출석체크/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 389);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(94, 38);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "출석체크 시작";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(24, 54);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(280, 246);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(24, 350);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(179, 33);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(20, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(506, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "출석명단 (띄어쓰기, 동명이인은 성까지 쓰기!)";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(572, 501);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 출석체크/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using Excel = Microsoft.Office.Interop.Excel; // 엑셀 lib 
+using System.IO;
+using System.Threading;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        
+        Excel.Application ap = null;
+        Excel.Workbook wb = null;
+        Excel.Worksheet ws = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String filepath = "C:\\Users\\사용자\\Desktop\\aa.xlsx";
+                if (filepath != null)
+                {
+                    ap = new Excel.Application(); // Excel 워크시트 가져오기 
+                    wb = ap.Workbooks.Open(filepath, 0, true, 5, "", "", true, Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                    ws = wb.Worksheets.get_Item("sheet1") as Excel.Worksheet; // 1번째 워크시트 
+              
+
+                    Excel.Range rg = ws.UsedRange; // 사용중인 엑셀 범위
+                    
+                    // 출석명단의 이름을 토큰 분리 
+                    char[] sep = { '\n', '\t', ' ' };
+                    string[] t_name = textBox2.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+
+                    int loading; // 로딩 %
+                    for (int i = 2; i < rg.Rows.Count; i++) // i는 이름이 있는 행번호
+                    {
+                        loading = 100 * i / rg.Rows.Count; // 100%로 표시하기 위해 설정
+                        textBox3.Text = loading.ToString() + "% 진행 중.."; // 두번째 창에 로딩 진행상황 표시
+
+                        if (ws.Cells[i] == null) continue; // 해당 행에 이름이 없으면 스킵
+                    
+                        for (int j = 0; j < t_name.Length; j++) // j는 출석명단에서 가져온 이름번호
+                        {
+                            if(t_name[j].Length == 1) // 이름이 외자일 경우에는 다른 이름에 출석이 체크될 수 있으니 2글자인 이름에만 출석을 진행한다.
+                            {
+                                if(ws.Cells[i, 1].Value.ToString().Contains(t_name[j]) && ws.Cells[i, 1].Value.ToString().Length == 2)
+                                {
+                                    ws.Cells[i, 3] = 1;
+                                    break;
+                                }
+
+                            }
+                            else if (ws.Cells[i,1].Value.ToString().Contains(t_name[j])) // 엑셀에 있는 이름에 출석명단이름이 포함되어 있으면 출석
+                            {
+                                ws.Cells[i , 3] = 1;
+                                break;
+                            }
+                        }
+                    }
+                    textBox3.Text = "출석체크 완료!";
+
+                    /*메모리 할당 해제*/
+                    DeleteObject(ws);
+                    DeleteObject(wb);
+                    ap.Quit();
+                    DeleteObject(ap);
+                    /*메모리 할당 해제*/
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("에러" + ex.Message, "에러!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DeleteObject(object obj)
+        {   // 메모리 해제를 위한 사용자 정의 함수
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("메모리 할당을 해제하는 중 문제가 발생하였습니다." + ex.ToString(), "경고!");
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+
+    }
+
+}
+
